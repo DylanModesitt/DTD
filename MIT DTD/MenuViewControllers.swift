4496d1079f992a2ff455b662118ae17f95c8a870
Dylan Modesitt <mod@mod.local>
2016-12-31 14:35:07 -0800

    Initial project setup. Dependency Setup. Basic storyboard. Authentication. Side navigation. Etc.

---
+++ MIT DTD/MenuViewControllers.swift
@@ -0,0 +1,9 @@
+//
+//  MenuViewControllers.swift
+//  MIT DTD
+//
+//  Created by Dylan Modesitt on 12/31/16.
+//  Copyright © 2016 Beta Nu Delta Tau Delta. All rights reserved.
+//
+
+import Foundation
