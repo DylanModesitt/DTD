36959361564addbe51d53275e9671ce4414b0803
Dylan Modesitt <mod@mod.local>
2016-12-31 12:24:24 -0800

    Initial Commit

---
+++ MIT DTDTests/MIT_DTDTests.swift
@@ -0,0 +1,36 @@
+//
+//  MIT_DTDTests.swift
+//  MIT DTDTests
+//
+//  Created by Dylan Modesitt on 12/31/16.
+//  Copyright © 2016 Beta Nu Delta Tau Delta. All rights reserved.
+//
+
+import XCTest
+@testable import MIT_DTD
+
+class MIT_DTDTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
